67714c0d916fc802bc0f7844ae1b5c563241d46f
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 20:23:40 +0530

    Creating a table named students

--- studentdb.sql
+++ studentdb.sql
@@ -3,3 +3,12 @@ CREATE DATABASE studentdb;
 
 -- Using database studentdb
 USE studentdb;
+
+-- Creating a table named students
+CREATE TABLE students (
+    name VARCHAR(100),
+    prn INT PRIMARY KEY,
+    branch VARCHAR(50),
+    batch VARCHAR(20),
+    cgpa FLOAT
+);
