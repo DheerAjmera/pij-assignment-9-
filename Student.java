61e6dedf531d26774c45e8784461f9320f385270
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 20:13:37 +0530

    Create Student.java
    initalize variables

---
+++ Student.java
@@ -0,0 +1,8 @@
+//Student.java
+
+// Student class to represent individual student records
+public class Student {
+    // Private fields for student details
+    private String name, branch, batch;
+    private int prn;
+    private float cgpa;
