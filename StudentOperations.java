814a80420359464eb2e84714ff97a482c0459a5c
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 20:20:13 +0530

    Update StudentOperations.java

--- StudentOperations.java
+++ StudentOperations.java
@@ -1,3 +1,8 @@
 //StudentOperations.java
 
 import java.util.*;
+// Handles operations related to students at the application level
+public class StudentOperations {
+
+    // Instance of StudentDAO to interact with the database
+    private StudentDAO dao;
