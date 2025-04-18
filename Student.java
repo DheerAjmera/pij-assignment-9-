5b0fb34d8f32c569e7ae56eadc8ef8dde9e04b84
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 20:14:00 +0530

    Constructor to initialize a student object with all attributes

--- Student.java
+++ Student.java
@@ -6,3 +6,11 @@ public class Student {
     private String name, branch, batch;
     private int prn;
     private float cgpa;
+    // Constructor to initialize a student object with all attributes
+    public Student(String name, int prn, String branch, String batch, float cgpa) {
+        this.name = name;
+        this.prn = prn;
+        this.branch = branch;
+        this.batch = batch;
+        this.cgpa = cgpa;
+    }
