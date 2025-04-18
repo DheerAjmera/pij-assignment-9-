085b76c0630e178053ca52e4c04b9c8f2877cdfb
Dheer Ajmera <165947269+DheerAjmera@users.noreply.github.com>
2025-04-18 20:25:31 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//Main.java
+
+//Name: Dheer Ajmera
+//PRN: 23070126035
+//Dept: SY-AIML-A2
+
+// Importing required classes
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);  // Scanner for user input
+        StudentOperations operations = new StudentOperations();  // Object to perform operations on students
+        int choice;
